e59ae7e7043c686a8d15891c131212bb5b0a57c6
like-ex <like@xtourtec.com>
2026-02-05 17:42:33 +0800

    add[ViewModel]在MyApplication里准备数据库仓库appDatabase;让CalendarViewModel// 通过application获取数据库appDatabase和DAO,创建Repository

--- app/src/main/java/com/surpasslike/calendar/MyApplication.kt
+++ app/src/main/java/com/surpasslike/calendar/MyApplication.kt
@@ -2,8 +2,14 @@ package com.surpasslike.calendar
 
 import android.app.Application
 import com.blankj.utilcode.util.Utils
+import com.surpasslike.calendar.data.database.AppDatabase
 
 class MyApplication : Application() {
+
+    // 整个APP只需要这一个数据库实例
+    val appDatabase: AppDatabase by lazy { // by lazy懒加载让数据库只在需要时才创建
+        AppDatabase.getInstance(this)
+    }
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/com/surpasslike/calendar/viewmodel/CalendarViewModel.kt
+++ app/src/main/java/com/surpasslike/calendar/viewmodel/CalendarViewModel.kt
@@ -1,7 +1,18 @@
 package com.surpasslike.calendar.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.surpasslike.calendar.MyApplication
+import com.surpasslike.calendar.repository.ScheduleRepository
 
-class CalendarViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class CalendarViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val scheduleRepository: ScheduleRepository
+
+    init {
+        // 通过application获取数据库appDatabase和DAO,创建Repository
+        val appDatabase = (application as MyApplication).appDatabase // 数据库实例,只会初始化一次,让整个应用只有一个数据库实例,避免数据混乱
+        val scheduleDao = appDatabase.scheduleDao() // 存取窗口,在Repository里执行具体的SQL操作
+        scheduleRepository = ScheduleRepository(scheduleDao) // 管理者,通过它在CalendarViewModel里调用增删改查
+    }
 }
